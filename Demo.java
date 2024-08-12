57684d29b99f02938053305331f0acffb621afa0
Rehaman14 <abdulrehaman0142@gmail.com>
2024-08-12 14:58:38 +0530

    Demo.java file commmited

---
+++ Demo.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Demo 
+{
+	public static void main(String[] args) {
+		int v1=200;
+		int v2=300;
+		int result=v1+v2;
+		System.out.println("total "+result);
+	}
+
+}
